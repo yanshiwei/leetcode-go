196a64252071701149264615a3ca69b0e53fc39a
cividyan <cividyan@tencent.com>
2020-12-06 17:14:32 +0800

    84

---
+++ 84_largeRectArea.go
@@ -0,0 +1,32 @@
+func largestRectangleArea(heights []int) int {
+    var res int
+    if len(heights)<1 {
+        return res
+    }
+    heights=append(heights,0)//hanlde [1] case
+    var st []int//asec stack get first smaller
+    for i:=range heights {//单调增套路
+        for len(st)>0&&heights[i]<=heights[st[len(st)-1]] {
+            var headIndex=st[len(st)-1]
+            var head=heights[headIndex]
+            st=st[0:len(st)-1]//pop//单调增right套路
+            //业务逻辑
+            var area int
+            if len(st)>0 {
+                area=head*(i-st[len(st)-1]-1)
+            }else{
+                area=head*i
+            }
+            res=max(res,area)
+        }
+        st=append(st,i)//单调增套路
+    }
+    return res
+}
+
+func max(x,y int)int {
+    if x<y {
+        return y
+    }
+    return x
+}
