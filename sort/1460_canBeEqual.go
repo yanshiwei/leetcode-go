395ebabcbed7c15b5e61b142e6853f2843d14bfb
cividyan <cividyan@tencent.com>
2021-04-11 14:22:32 +0800

    1460

---
+++ sort/1460_canBeEqual.go
@@ -0,0 +1,10 @@
+func canBeEqual(target []int, arr []int) bool {
+    sort.Ints(target)
+    sort.Ints(arr)
+    for i:=range target{
+        if target[i]!=arr[i]{
+            return false
+        }
+    }
+    return true
+}
