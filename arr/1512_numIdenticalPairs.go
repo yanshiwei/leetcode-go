567c0f2fd802e03a711fd71f8b2d0611e9da1884
cividyan <cividyan@tencent.com>
2021-04-21 22:01:45 +0800

    1512

---
+++ arr/1512_numIdenticalPairs.go
@@ -0,0 +1,11 @@
+func numIdenticalPairs(nums []int) int {
+    var res int
+    for i:=0;i<len(nums)-1;i++{
+        for j:=i+1;j<len(nums);j++{
+            if nums[i]==nums[j]{
+                res++
+            }
+        }
+    }
+    return res
+}
