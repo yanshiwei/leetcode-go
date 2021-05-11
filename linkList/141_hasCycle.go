3ed39aab0b0f7471610152ed52e35ad1ab3af9cb
cividyan <cividyan@tencent.com>
2021-05-11 22:17:46 +0800

    141

---
+++ linkList/141_hasCycle.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    if head==nil||head.Next==nil{
+        return false
+    }
+    var fast=head
+    var slow=head
+    for fast!=nil&&fast.Next!=nil{
+        fast=fast.Next.Next
+        slow=slow.Next
+        if fast==slow{
+            return true
+        }
+    }
+    return false
+}
