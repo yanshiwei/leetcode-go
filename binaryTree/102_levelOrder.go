876eb4682de6dc49e12a7e556b503f4cc0c2f934
cividyan <cividyan@tencent.com>
2021-05-27 23:09:28 +0800

    102

---
+++ binaryTree/102_levelOrder.go
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+    return levelOrderByIteration(root)
+}
+
+func levelOrderByIteration(root *TreeNode) [][]int {
+    var res [][]int
+    if root==nil{
+        return res
+    }
+    var queue []*TreeNode
+    queue=append(queue,root)
+    for len(queue)>0{
+        curLen:=len(queue)
+        var tmp []int
+        for i:=0;i<curLen;i++{
+            cur:=queue[0]
+            queue=queue[1:]
+            tmp=append(tmp,cur.Val)
+            if cur.Left!=nil{
+                queue=append(queue,cur.Left)
+            }
+            if cur.Right!=nil{
+                queue=append(queue,cur.Right)
+            }
+        }
+        res=append(res,tmp)
+    }
+    return res
+}
