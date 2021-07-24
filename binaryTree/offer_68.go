3e0c8c19df52c237140ba6491c8c9ef4f2cb8229
yanshiwei <2469327177@qq.com>
2021-07-24 11:23:12 +0800

    offer 68

---
+++ binaryTree/offer_68.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(1) {
+            //all in left
+            if (p->val<root->val&&q->val<root->val){
+                root=root->left;
+            }else if (p->val>root->val&&q->val>root->val){
+                //all in right
+                root=root->right;
+            }else{
+                break;
+            }
+        }
+        return root;
+    }
+};
