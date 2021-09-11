35eb99a45b240e266ea904a107bb66aec9e74832
yanshiwei <2469327177@qq.com>
2021-09-11 12:49:56 +0800

    70

---
+++ dp/70_climbStairs.go
@@ -0,0 +1,14 @@
+func climbStairs(n int) int {
+    //dp
+    var res int
+    if n<1{
+        return res
+    }
+    var dp=make([]int,n+1)
+    dp[0]=1
+    dp[1]=1
+    for i:=2;i<=n;i++{
+        dp[i]=dp[i-1]+dp[i-2]
+    }
+    return dp[n]
+}
