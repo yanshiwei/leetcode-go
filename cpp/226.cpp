2f4f58e6116208afbebb976d4a93fc4a03fcb8ad
yanshiwei <2469327177@qq.com>
2022-09-17 18:13:07 +0800

    226

---
+++ cpp/226.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    /*
+    给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+    */
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr){
+            return nullptr;
+        }
+        if(root->left==nullptr&&root->right==nullptr){
+            return root;
+        }
+        root->left=invertTree(root->left);
+        root->right=invertTree(root->right);
+        TreeNode*tmp=root->left;
+        root->left=root->right;
+        root->right=tmp;
+        return root;
+    }
+};
