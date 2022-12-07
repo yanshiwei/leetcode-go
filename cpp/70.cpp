92a52c1c92a42f79c6394eb1b5074e640f1a5df3
yanshiwei <2469327177@qq.com>
2022-12-07 21:18:44 +0800

    70

---
+++ cpp/70.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    // 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+    // 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+    // dp[i]第i个台阶的走法数=dp[i-1]+dp[i-2]
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
