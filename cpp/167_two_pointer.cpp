a8d32be36f47c9d6c5c8fcaf9591a4c9ec7d7620
yanshiwei <2469327177@qq.com>
2023-07-25 21:45:48 +0800

    167 two pointer

---
+++ cpp/167_two_pointer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    /*
+    给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
+以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+    */
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        //相向指针
+        if(numbers.size()<1){
+            return {};
+        }
+        int left=0;
+        int right=numbers.size()-1;
+        while(left<right){
+            //不可以 重复使用相同的元素故这里left<right
+            int sum=numbers[left]+numbers[right];
+            if(sum==target){
+                return {left+1,right+1};//下标从1开始
+            }else if(sum>target){
+                // 过大
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return {};
+    }
+};
