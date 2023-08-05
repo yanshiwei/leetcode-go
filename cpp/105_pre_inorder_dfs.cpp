0e1a1d84f91e71ff5b62011bda77a87a1f6581e5
yanshiwei <2469327177@qq.com>
2023-08-05 17:05:14 +0800

    105 dfs

---
+++ cpp/105_pre_inorder_dfs.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    /*
+    给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+    */
+public:
+    TreeNode* helper(vector<int>& preorder,int p_start,int p_end,vector<int>& inorder,int i_start,int i_end){
+        if(p_start==p_end){
+            return nullptr;
+        }
+        int root_val=preorder[p_start];
+        TreeNode*root=new TreeNode(root_val);
+        //在中序遍历中找到根节点的位置
+        int idx=-1;
+        for(int i=0;i<inorder.size();i++){
+            if(inorder[i]==root_val){
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1){
+            return nullptr;
+        }
+        int leftNum=idx-i_start;
+        //递归的构造左子树
+        root->left=helper(preorder,p_start+1,p_start+1+leftNum,inorder,i_start,idx);
+        root->right=helper(preorder,p_start+1+leftNum,p_end,inorder,idx+1,i_end);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
+    }
+};
