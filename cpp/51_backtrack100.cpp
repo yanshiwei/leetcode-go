56c945d475bd1df6c950eacfbd40ab4b26e4d12a
yanshiwei <2469327177@qq.com>
2023-08-20 18:40:50 +0800

    51 backtrack 100

--- cpp/51_backtrack100.cpp
+++ cpp/51_backtrack100.cpp
@@ -1,7 +1,13 @@
 class Solution {
-    // https://leetcode.cn/problems/n-queens/solutions/399075/nhuang-hou-jing-dian-hui-su-suan-fa-tu-wen-xiang-j/
+    /*
+    题目：
+    n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+    题解：
+    https://leetcode.cn/problems/n-queens/solutions/399075/nhuang-hou-jing-dian-hui-su-suan-fa-tu-wen-xiang-j/
+    DFS
+    */
 public:
-    // 判断当前行 列 是否满足
+    //  判断 当前行下，该列，能否放入
     bool isValid(vector<string>&queens,int curRow,int col){
         // 相同列之前没有Q
         for(int i=0;i<curRow;i++){
@@ -9,20 +15,25 @@ public:
                 return false;
             }
         }
-        // 每一行只放一个Q，故在判断此行是否可以放时，次行之前还没放，故无需判断相同行
-        // 当前行列 左对角线之前有没有Q
+
+        // 每一行只放一个Q，故不需要判断相同行
+        // nothing todo
+
+        // 左对角线之前有没有Q
         for(int i=curRow-1,j=col-1;i>=0&&j>=0;i--,j--){
             if(queens[i][j]=='Q'){
                 return false;
             }
         }
-        // 当前行列 右对角线之前有没有Q
+
+        // 右对角线之前有没有Q
         for(int i=curRow-1,j=col+1;i>=0&&j<queens.size();i--,j++){
             if(queens[i][j]=='Q'){
                 return false;
             }
         }
-        return true;
+
+        return true;      
     }
     void helper(vector<string>&queens,int curRow,vector<vector<string>> &res){
         if(curRow==queens.size()){
@@ -39,10 +50,9 @@ public:
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-      vector<vector<string>> res;
-      vector<string>queens(n,string(n,'.'));
-      // 从0行开始递归
-      helper(queens,0,res);
-      return res;  
+        vector<vector<string>> res;
+        vector<string>queens(n,string(n,'.'));
+        helper(queens,0,res);
+        return res;
     }
 };
