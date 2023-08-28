e59529a9d90f4de873248cb15de8065f23a36c86
yanshiwei <2469327177@qq.com>
2023-08-28 22:53:07 +0800

    274 bs 100

--- cpp/274_bad_bs.cpp
+++ cpp/274_bad_bs.cpp
@@ -1,40 +1,50 @@
 class Solution {
     // 二分https://leetcode.cn/problems/h-index/solutions/870465/er-fen-cai-lun-wen-pian-shu-java-by-liwe-zoh7/
+    /*
+    类似287
+    我们知道，一个数组在某个属性上有单调性，就可以进行二分查找
+（最明显就是排序数组的数组值）。对于本题来说还有什么单调性呢。我们尝试探索一下
+    例如arr=[1 3 5 7 10 13]
+    引用次数>=1，文章篇数=6>1
+    引用次数>=3，文章篇数=5>3
+    引用次数>=5，文章篇数=4<5
+    引用次数>=7，文章篇数=3<7
+    引用次数>=10，文章篇数=2<10
+    引用次数>=13，文章篇数=1<13
+    所以引用次数越大越大，超过该引用的文章篇数就越少，我们记这个属性为cnt,则现在我们
+知道，对于【0, 1，2，3,...n】这个数组值升序数组来说，数组的cnt属性值是降序的
+    对于目标target，它的cnt属性有什么特点呢。引用次数>=target，文章篇数>=target
+    if 引用次数<=target,则引用次数的cnt属性值也就是文章篇数>=引用次数
+    if 引用次数>target,则引用次数的cnt属性值也就是文章篇数<引用次数
+    根据这个结论，我们就可以进行二分查找
+    */
 public:
     int hIndex(vector<int>& citations) {
         //1 3 | 5 7 10 13
-        // 引入次数>=4的有4篇，其余2篇引用次数不大于4，故h=4
-        // 可以看到h=4时将数组划分2段，满足二分特性
-        // 使用二分确定范围：
-        // 1 最差情况下，所有的论文被引用次数都为 0；
-        // 2 最好情况下，所有的论文的引用次数 >= 总共论文篇数
-        // 找先猜一个论文篇数 int mid = (left + right + 1) / 2
-        // 如果大于等于 mid 的论文篇数大于等于 mid ，说明 h 指数至少是 mid。例如 [0,1,|3,5,6]，引用次数大于等于 2 的论文有 3 篇，说明答案至少是 2，最终答案落在区间 [mid..right] 里，此时设置 left = mid；
-        // 反面的情况不用思考，因为只要上面分析对了，最终答案落在区间 [mid..right] 的反面区间 [left..mid - 1] 里，此时设置 right = mid - 1
         int n=citations.size();
         int left=0;//最差情况下，所有的论文被引用次数都为 0
         int right=n;//最好情况下，所有的论文的引用次数 >= 总共论文篇数
         while(left<right){
             // 若等于，left==right。走到1步骤时候会循环
             int mid=left+(right-left)/2+1;//这里+1，否则[1]会循环
-            if(check(citations,mid)){
-                // 1 in the right
+            if(check(citations,mid)>=mid){
+                // target在 in the right [mid,right]
                 left=mid;
             }else{
-                // 2 in the left
+                // target在 in the left [left,mid-1]
                 right=mid-1;
             }
         }
         return left;
     }
 private:
-    bool check(vector<int>&citations,int mid){
+    int check(vector<int>&citations,int mid){
         int cnt=0;
         for(auto &n:citations){
             if(n>=mid){
                 cnt++;
             }
         }
-        return cnt>=mid;
+        return cnt;
     }
 };
