b99d85822bea877fd41658ba22490e6be28ad9f7
yanshiwei <2469327177@qq.com>
2023-09-03 20:20:50 +0800

    209 windows like 3 two pointers 150

--- cpp/209_two_pointer150.cpp
+++ cpp/209_two_pointer150.cpp
@@ -4,23 +4,26 @@ class Solution {
 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
     */
 public:
+    const int int_max=0x7fffffff;
     int minSubArrayLen(int target, vector<int>& nums) {
-        int res=0x7fffffff;
+        int res=int_max;
         int left=0,right=0;
         int sum=0;
         while(right<nums.size()){
-            sum+=nums[right];
-            while(sum>=target){
-                // 最小长度
-                res=min(res,right-left+1);
-                sum-=nums[left];
-                left++;
-            }
+            int c=nums[right];
             right++;
+            // 窗口更新后操作
+            sum+=c;
+            // 窗口左移
+            while(sum>=target){
+                // sum>=target时，更新长度
+                res=min(res,right-left);
+                int d=nums[left];
+                left++;
+                // 窗口更新后操作
+                sum-=d;
+            }
         }
-        if(res==0x7fffffff){
-            return 0;
-        }
-        return res;
+        return res==int_max?0:res;
     }
 };
